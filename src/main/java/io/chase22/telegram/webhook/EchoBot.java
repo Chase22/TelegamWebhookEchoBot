666c787f30b56ffb1fa127c9798d24648ea10e96
Chase22 <chase@mailbox.org>
2020-04-20 19:21:55 +0200

    Add basic bot

---
+++ src/main/java/io/chase22/telegram/webhook/EchoBot.java
@@ -0,0 +1,32 @@
+package io.chase22.telegram.webhook;
+
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class EchoBot extends TelegramWebhookBot {
+    @Override
+    public BotApiMethod onWebhookUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            return new SendMessage(update.getMessage().getChatId(), update.getMessage().getText());
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return System.getenv("BOT_USERNAME");
+    }
+
+    @Override
+    public String getBotToken() {
+        return System.getenv("BOT_TOKEN");
+    }
+
+    @Override
+    public String getBotPath() {
+        return System.getenv("BOT_PATH");
+    }
+}
