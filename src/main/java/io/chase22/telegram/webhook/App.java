920905b1aa0a508bf390780368887543155141dc
Chase22 <chase@mailbox.org>
2020-04-20 19:23:23 +0200

    initialize bot

--- src/main/java/io/chase22/telegram/webhook/App.java
+++ src/main/java/io/chase22/telegram/webhook/App.java
@@ -3,12 +3,15 @@
  */
 package io.chase22.telegram.webhook;
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+public class App {
+    public static void main(String[] args) throws TelegramApiRequestException {
+        ApiContextInitializer.init();
+        TelegramBotsApi api = new TelegramBotsApi();
+
+        api.registerBot(new EchoBot());
     }
 }
